e6414ef10d05e50303d5bbc2157f0dc6d9a48c04
saliqasarwi <sali.qasarwi@gmail.com>
2025-11-05 17:53:07 +0200

    add a basic implementation to calc with validation

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
-function calc(){
-    throw new Error("Not emplemented yet");
+function calc(a,op,b){
+if(typeof a!=='number'||typeof b!=='number'){
+    throw new Error ('Invalid input type');
 }
+switch(op){
+    case '+':return a+b;
+    case '-':return a-b;
+    case '*':return a*b;
+    case '/':
+        if(b===0) throw new Error('Division by zero');
+        return a/b;
+    default:
+        throw new Error('Invalid operator');    
+}
+}
+module.exports = calc;
