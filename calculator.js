1e2211ee242d856ec8e2f4a568a2866fed5b12fd
saliqasarwi <sali.qasarwi@gmail.com>
2025-11-05 21:49:52 +0200

    ignore numbers>1000 in calculations

--- calculator.js
+++ calculator.js
@@ -8,21 +8,28 @@ for(let i=0;i<args.length;i++){
     if(i%2===0){
         if(typeof args[i]!=='number')
              throw new Error('Invalid input type');
-        numbers.push(args[i]);
+        if(args[i]<=1000)numbers.push(args[i]);
+        else if(numbers.length>0)
+           {
+            operators.pop();
+            }
+        
     }else{
         if(typeof args[i]!=='string') 
             throw new Error('Invalid input type');
         if (!['+','-','*','/'].includes(args[i])){
             throw new Error('Invalid operator');
         }
-        operators.push(args[i]);
+        if(i>0&&args[i-1]<=1000)operators.push(args[i]);
     }
 }
+if(numbers.length===0)return 0;
+if(numbers.length===1)return numbers[0];
 for(let i=0;i<operators.length;i++){
     if(operators[i]==='*'||operators[i]==='/'){
         const left=numbers[i];
         const right=numbers[i+1];
-        if(operands[i]==='/'&&right===0) throw new Error('Division by zero');
+        if(operators[i]==='/'&&right===0) throw new Error('Division by zero');
         const result=operators[i]==='*'?left*right:left/right;
         numbers[i]=result;
         numbers.splice(i+1,1);
